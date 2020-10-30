ef0354d0d9209e6fe6c6048c97ef6e5ca38c882e
L-ENA <lena.schmidt.0493@gmail.com>
2020-10-30 12:45:33 +0000

    add

---
+++ commands.R
@@ -0,0 +1,12 @@
+#To build the website, run the function wflow_build() in the R console:
+wflow_build()
+#To view the site without first building any files, run wflow_view(), which by default displays the file docs/index.html:
+wflow_view()
+wflow_status()
+#add new pages as Rmd, and then add it to site.yml in the analysis direc
+wflow_open("analysis/Latest Updates.Rmd")
+
+
+wflow_build()
+wflow_publish(c("analysis/*", "*"),
+              "add")
