8dadf0ae08c076a53bb152d8989ac6a3bda19b2d
L-ENA <lena.schmidt.0493@gmail.com>
2020-10-30 14:05:07 +0000

    Updated at 2020-10-30 14:04:58

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # website
 
-A [workflowr][] project. This is the website for a living review.
+A [workflowr][] project. This is the website for a living review, showing live updates between each publication.
 
 [workflowr]: https://github.com/jdblischak/workflowr
--- commands.R
+++ commands.R
@@ -1,15 +1,30 @@
 library(knitr)    # For knitting document and include_graphics function
+library(glue)
+wflow_git_pull(username = "L-ENA",
+               password = "lukas1997-LNQGQGC4")
 
-
+#wflow_use_github("L-ENA")
 #To build the website, run the function wflow_build() in the R console:
-wflow_build()
+
 #To view the site without first building any files, run wflow_view(), which by default displays the file docs/index.html:
-wflow_view()
-wflow_status()
+#wflow_view()
+#wflow_status()
 #add new pages as Rmd, and then add it to site.yml in the analysis direc
-wflow_open("analysis/Latest Updates.Rmd")
+#wflow_open("analysis/Latest Updates.Rmd")
+#wflow_git_push(dry_run = TRUE)
 
 
 wflow_build()
+
+mytime=Sys.time()
+commit_msg=glue("Updated at {mytime}")
+
 wflow_publish(c("analysis/*", "*"),
-              "Updated index")
+              commit_msg)
+
+
+
+wflow_git_push(username = "L-ENA",
+               password = "lukas1997-LNQGQGC4")
+
+
